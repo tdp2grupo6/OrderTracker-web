496fd76df94ba9fbbad22fa567394c921cb6d88e
Belen <belubeltran@gmail.com>
2016-04-25 17:25:06 -0300

    US-297: Pantalla de admin de pedidos con grilla editable y datos linkeados. Falta linkear las acciones de los botones

--- src/app/main/listadoPedidos.controller.js
+++ src/app/main/listadoPedidos.controller.js
@@ -3,18 +3,45 @@
 
     angular
     	.module('myApp')
-      .controller('ListadoPedidosController', ListadoPedidosController);
+      .controller('ListadoPedidosController', ListadoPedidosController)
+      .run(function(editableOptions) {
+        editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+      });;
+        
+      ListadoPedidosController.$inject = ['$scope', '$mdDialog', 'Pedidos', '$filter'];
 
-      ListadoPedidosController.$inject = ['$scope', '$mdDialog', 'Pedidos'];
-
-      function ListadoPedidosController($scope, $mdDialog, Pedidos) {
+      function ListadoPedidosController($scope, $mdDialog, Pedidos, $filter) {
 
         Pedidos.listarTodosPedidos(null,
             function(data) {
-              $scope.pedidos = data
+              $scope.pedidos = data;
             },
             function() {
+
             }
         );
+
+        $scope.estados = [{id:1,tipo:'ESTADO_CONFIRMADO',nombre:'Confirmado'},{id:2,tipo:'ESTADO_ENVIADO',nombre:'Enviado'}];
+
+        $scope.showEstados = function(pedido) {
+          if(pedido.estado && $scope.estados.length) {
+            var selected = $filter('filter')($scope.estados, {id: pedido.estado.id});
+            return selected.length ? selected[0].nombre : pedido.estado.nombre;
+          } else {
+            return pedido.estado.nombre || 'No anda te dije';
+          }
+        };
+
+        $scope.removeProducto = function($index) {
+          Pedidos.borrarPedido($index, 
+            function(data) {
+              $scope.pedidos = data;
+            },
+            function() {
+
+            }
+            );
+        };
+
       }
 })();
