3908f66151f6c21c1572581db4c08646284d4d7b
mroitman <silverhawks.22@gmail.com>
2016-05-07 19:28:06 -0300

    cambio de inyeccion de css

--- gulp/inject.js
+++ gulp/inject.js
@@ -9,8 +9,8 @@ var wiredep = require('wiredep').stream;
 module.exports = function(options) {
   gulp.task('inject', ['scripts', 'styles'], function () {
     var injectStyles = gulp.src([
-      options.tmp + '/serve/app/**/*.scss',
-      '!' + options.tmp + '/serve/app/vendor.scss'
+      options.tmp + '/serve/app/**/*.css',
+      '!' + options.tmp + '/serve/app/vendor.css'
     ], { read: false });
 
     var injectScripts = gulp.src([
