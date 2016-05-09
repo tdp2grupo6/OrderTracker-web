f2cef58d7355c92802d101a6d98a26ff82b780fa
Daniel Gacitúa <danielgacituav@gmail.com>
2016-05-08 21:44:55 -0300

    Traducción de los DatePickers

--- src/app/index.js
+++ src/app/index.js
@@ -11,7 +11,7 @@ angular.module('myApp', [
     'ngMessages',
     'xeditable',
     'md.data.table'])
-    
+
     .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider) {
         $stateProvider
             .state('main', {
@@ -31,7 +31,7 @@ angular.module('myApp', [
             });
 
         $urlRouterProvider.otherwise('/main');
-        $locationProvider.html5Mode(true).hashPrefix('#');  
+        $locationProvider.html5Mode(true).hashPrefix('#');
 
         $mdThemingProvider.theme('default')
             .primaryPalette('blue')
@@ -45,10 +45,25 @@ angular.module('myApp', [
     .run(function($rootElement) {
         $rootElement.on('click', function(e) { e.stopPropagation(); });
     })
+
+    // dgacitua: Traducción de DatePickers
+    .config(function($mdDateLocaleProvider) {
+      $mdDateLocaleProvider.months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+      $mdDateLocaleProvider.shortMonths = ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'];
+      $mdDateLocaleProvider.days = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+      $mdDateLocaleProvider.shortDays = ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'];
+      $mdDateLocaleProvider.firstDayOfWeek = 0;
+      // In addition to date display, date components also need localized messages
+      // for aria-labels for screen-reader users.
+      $mdDateLocaleProvider.weekNumberFormatter = function(weekNumber) {
+        return 'Semana ' + weekNumber;
+      };
+      $mdDateLocaleProvider.msgCalendar = 'Calendario';
+      $mdDateLocaleProvider.msgOpenCalendar = 'Abrir el calendario';
+    })
 ;
 
 
 
-    
 
-    
+
