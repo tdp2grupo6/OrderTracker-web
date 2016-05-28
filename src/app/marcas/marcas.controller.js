3ffd993e14cbdaa8266d90923258c95ddd5f3e90
Daniel Gacitúa <danielgacituav@gmail.com>
2016-05-28 00:06:02 -0300

    Corregido bug de no poder eliminar una Marca

--- src/app/marcas/marcas.controller.js
+++ src/app/marcas/marcas.controller.js
@@ -78,152 +78,6 @@
             .join(' ');
         };
 
-        // Ver Marca Modal
-      /*
-        $scope.mostrarDetalleMarca = function(ev, id) {
-          var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
-
-          Marcas.listarMarcas({ id: id },
-            function(data) {
-              $scope.marca = data;
-
-              // Mostrar Modal
-              $mdDialog.show({
-                  templateUrl: 'app/marca/marcaDetalle.tmpl.html',
-                  targetEvent: ev,
-                  scope: $scope.$new(),
-                  clickOutsideToClose:true,
-                  fullscreen: useFullScreen,
-                  onRemoving: function() { $scope.cerrarModal() }
-                })
-                .then(function(answer) {
-                }, function() {
-                });
-
-              $scope.$watch(function() {
-                return $mdMedia('xs') || $mdMedia('sm');
-              }, function(wantsFullScreen) {
-                $scope.customFullscreen = (wantsFullScreen === true);
-              });
-            },
-            function(error) {
-              console.log("ERROR! No se puede mostrar la Marca " + error);
-              $mdToast.show($mdToast.simple().textContent('ERROR! No se puede mostrar la Marca').position($scope.getToastPosition()).hideDelay(3000))
-            }
-          );
-        };
-        */
-
-        /*
-        $scope.editarMarcaModal = function(ev,id) {
-          var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
-
-          Marcas.listarMarcas({ id: id },
-            function(data) {
-              $scope.marca = data;
-
-              // Mostrar Modal
-              $mdDialog.show({
-                  templateUrl: 'app/clientes/editarMarca.tmpl.html',
-                  targetEvent: ev,
-                  scope: $scope.$new(),
-                  clickOutsideToClose:true,
-                  fullscreen: useFullScreen,
-                  onRemoving: function() { $scope.cerrarModal() }
-                })
-                .then(function(answer) {
-
-                }, function() {
-                });
-
-              $scope.$watch(function() {
-                return $mdMedia('xs') || $mdMedia('sm');
-              }, function(wantsFullScreen) {
-                $scope.customFullscreen = (wantsFullScreen === true);
-              });
-            },
-            function(error) {
-              console.log("ERROR! No se puede mostrar la Marca " + error);
-              $mdToast.show($mdToast.simple().textContent('ERROR! No se puede mostrar la Marca').position($scope.getToastPosition()).hideDelay(3000))
-            }
-          );
-        };
-
-
-        $scope.borrarMarca = function(id) {
-           Marcas.borrarMarca({ id: id },
-            function() {
-             $mdToast.show($mdToast.simple().textContent('La Marca ha sido borrada Satisfactoriamente').position($scope.getToastPosition()).hideDelay(3000));
-                Marcas.filtrarMarca($scope.query,
-                function(data) {
-                  $scope.marcas = data.resultados;
-                  $scope.totalResultados = data.totalResultados;
-                  angular.forEach($scope.marcas, function(marca) {});
-                },
-                function() {
-
-                }
-              );
-            },
-            function() {
-              $mdToast.show($mdToast.simple().textContent('La Marca no pudo ser borrada').position($scope.getToastPosition()).hideDelay(3000));
-            }
-          );
-        };
-
-        $scope.submit = function() {
-          var geocoder = new google.maps.Geocoder();
-          geocoder.geocode( { "address": $scope.form.direccion }, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
-              var location = results[0].geometry.location;
-              $scope.form.latitud = location.lat();
-              $scope.form.longitud = location.lng();
-            }
-
-            Clientes.guardarCliente($scope.form,
-              function() {
-                $mdToast.show($mdToast.simple().textContent('El Cliente ha sido agregado Satisfactoriamente').position($scope.getToastPosition()).hideDelay(3000));
-                $scope.cerrarModal();
-              },
-              function() {
-                $mdToast.show($mdToast.simple().textContent('El Cliente no pudo ser agregado').position($scope.getToastPosition()).hideDelay(3000));
-              }
-            );
-          });
-        };
-
-
-        $scope.update = function(id) {
-          var geocoder = new google.maps.Geocoder();
-          geocoder.geocode( { "address": $scope.cliente.direccion }, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
-              var location = results[0].geometry.location;
-              $scope.cliente.latitud = location.lat();
-              $scope.cliente.longitud = location.lng();
-            }
-
-            $scope.request = angular.copy($scope.cliente);
-
-            delete $scope.request.id;
-            delete $scope.request.nombreCompleto;
-            delete $scope.request.estado;
-            delete $scope.request.validador;
-            delete $scope.request.agendaCliente;
-
-            console.log($scope.request);
-
-          Clientes.actualizarCliente({id: id},$scope.request,
-              function() {
-                $mdToast.show($mdToast.simple().textContent('El Cliente ha sido actualizado Satisfactoriamente').position($scope.getToastPosition()).hideDelay(3000));
-                $scope.cerrarModal();
-              },
-              function() {
-                $mdToast.show($mdToast.simple().textContent('El Cliente no pudo ser actualizado').position($scope.getToastPosition()).hideDelay(3000));
-              }
-            );
-          });
-        };*/
-
         $scope.agregarMarcasModal = function(ev) {
           var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
 
@@ -244,7 +98,40 @@
           }, function(wantsFullScreen) {
            $scope.customFullscreen = (wantsFullScreen === true);
           });
+        };
 
+        $scope.editarMarcaModal = function(ev,id) {
+          var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+
+          Marcas.listarMarca({ id: id },
+            function(data) {
+              $scope.marca = data;
+
+              // Mostrar Modal
+              $mdDialog.show({
+                  templateUrl: 'app/marcas/editarMarcas.tmpl.html',
+                  targetEvent: ev,
+                  scope: $scope.$new(),
+                  clickOutsideToClose:true,
+                  fullscreen: useFullScreen,
+                  onRemoving: function() { $scope.cerrarModal() }
+                })
+                .then(function(answer) {
+
+                }, function() {
+                });
+
+              $scope.$watch(function() {
+                return $mdMedia('xs') || $mdMedia('sm');
+              }, function(wantsFullScreen) {
+                $scope.customFullscreen = (wantsFullScreen === true);
+              });
+            },
+            function(error) {
+              console.log("ERROR! No se puede mostrar la Marca " + error);
+              $mdToast.show($mdToast.simple().textContent('ERROR! No se puede mostrar la Marca').position($scope.getToastPosition()).hideDelay(3000))
+            }
+          );
         };
 
         $scope.getMarcasFiltrado = function (pagina, limite) {
@@ -351,51 +238,25 @@
           }
         };
 
-        $scope.editarMarcaModal = function(ev,id) {
-          var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.borrarMarca = function(id) {
+          Marcas.borrarMarca({ id: id },
+            function() {
+              $mdToast.show($mdToast.simple().textContent('La Marca ha sido borrada Satisfactoriamente').position($scope.getToastPosition()).hideDelay(3000));
+              Marcas.filtrarMarca($scope.query,
+                function(data) {
+                  $scope.marcas = data.resultados;
+                  $scope.totalResultados = data.totalResultados;
+                  angular.forEach($scope.marcas, function(marca) {});
+                },
+                function() {
 
-          Marcas.listarMarca({ id: id },
-            function(data) {
-              $scope.marca = data;
-
-              // Mostrar Modal
-              $mdDialog.show({
-                  templateUrl: 'app/marcas/editarMarcas.tmpl.html',
-                  targetEvent: ev,
-                  scope: $scope.$new(),
-                  clickOutsideToClose:true,
-                  fullscreen: useFullScreen,
-                  onRemoving: function() { $scope.cerrarModal() }
-                })
-                .then(function(answer) {
-
-                }, function() {
-                });
-
-              $scope.$watch(function() {
-                return $mdMedia('xs') || $mdMedia('sm');
-              }, function(wantsFullScreen) {
-                $scope.customFullscreen = (wantsFullScreen === true);
-              });
+                }
+              );
             },
-            function(error) {
-              console.log("ERROR! No se puede mostrar la Marca " + error);
-              $mdToast.show($mdToast.simple().textContent('ERROR! No se puede mostrar la Marca').position($scope.getToastPosition()).hideDelay(3000))
+            function() {
+              $mdToast.show($mdToast.simple().textContent('La Marca no pudo ser borrada').position($scope.getToastPosition()).hideDelay(3000));
             }
           );
         };
-
-        /*
-        $scope.uploadFile = function(){
-          var file = $scope.myFile;
-          var filename = 'imagen';
-          var uploadUrl = 'http://localhost:8080/OrderTracker/imagen/subir';
-          UploadFile.uploadFileToUrl(file, uploadUrl, filename)
-            .success(function(data) {
-              $scope.imagen = data;
-              console.log($scope.imagen);
-            });
-        };
-       */
       }
 })();
